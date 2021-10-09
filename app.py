23b2d9a759cbe805f65cd7a8f3e584c30c4d00c3
cecyRdz <92197205+cecyRdz@users.noreply.github.com>
2021-10-09 13:53:50 -0500

    Otros pasos
    Se agrego un nuevo archivo html en categorias, se agrego en el index principal, todo esto para meras cuestiones de observar el enrutamiento

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ def inicio():
     #return'<h1>HOLA CECY</h1>'
     return render_template('Comunes/index.html')
 
+@app.route('/Categorias')
+def listarCategorias():
+    return render_template('categorias/ListarCategorias.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
