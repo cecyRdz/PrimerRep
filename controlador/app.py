ee565d77332f9471b0d911c1591763b1381804a8
cecyRdz <92197205+cecyRdz@users.noreply.github.com>
2021-10-10 06:22:36 -0500

    Primera práctica de productos

--- controlador/app.py
+++ controlador/app.py
@@ -10,5 +10,13 @@ def inicio():
 def listarCategorias():
     return render_template('categorias/ListarCategorias.html')
 
+@app.route('/ModificarProductos')
+def ModificarProductos():
+    return render_template('Productos/ModificarProductos.html')
+
+@app.route('/ListadoProductos')
+def ListadoProductos():
+    return render_template('Productos/ListadoProductos.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
