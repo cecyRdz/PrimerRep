af6b38440a408863813c27155081ead24db4075b
cecyRdz <92197205+cecyRdz@users.noreply.github.com>
2021-11-16 20:54:15 -0600

    Consultar, registrar y modificar es posible!!
    Falta la parte de imagenes y la eliminar

--- controlador/app.py
+++ controlador/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request,flash
 from flask_bootstrap import Bootstrap
 from modelo.DAO import db, Productos
 
@@ -7,6 +7,7 @@ Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://userShopitesz:Hola.123@localhost/shopitesz_v2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.secret_key='cl4v3'
 
 @app.route('/')
 def inicio():
@@ -24,21 +25,53 @@ def registrarProducto():
 
 @app.route('/registrarNuevoProducto',methods=['post'])
 def registrarNuevoProducto():
-    nombre = request.form['nombre']
-    return 'Se ha registrado el producto: '+nombre
-
-@app.route('/ModificarProducto')
-def ModificarProducto():
-    return render_template('Productos/editarProducto.html')
-
-@app.route('/recopilarDatosEditados',methods=['post'])
-def recopilarDatosEditados():
-    nombre = request.form['nombre']
-    return 'Se guardaron los cambios del producto: '+nombre
+    p = Productos()
+    p.IDCATEGORIA=request.form['idCategoria']
+    p.NOMBRE=request.form['nombre']
+    p.DECRIPCION=request.form['descripcion']
+    p.PRECIO=request.form['precioVenta']
+    p.EXISTENCIA=request.form['existencia']
+    p.COLOR=request.form['color']
+    p.MARCA=request.form['marca']
+    p.COSTOENVIO=request.form['costoEnvio']
+    #p.ESTATUS=request.form['estatus']
+    p.foto=request.files['foto'].read()
+    p.insertar()
+    flash('Se ha registrado un nuevo producto con éxito!!')
+    return render_template('Productos/registrarProducto.html')
 
 @app.route('/EliminarProducto')
 def EliminarProducto():
-    return render_template('Productos//eliminarProducto.html')
+    return render_template('Productos/eliminarProducto.html')
+
+@app.route('/productos/ver/<int:id>')
+def consultarProducto(id):
+    p = Productos()
+    return render_template('Productos/editarProducto.html', producto=p.consultaIndividual(id))
+
+@app.route('/guardarDatosProducto',methods=['post'])
+def guardarDatosProducto():
+    p = Productos()
+    p.IDPRODUCTO=request.form['idProducto']
+    p.IDCATEGORIA=request.form['idCategoria']
+    p.NOMBRE=request.form['nombre']
+    p.DECRIPCION=request.form['descripcion']
+    p.PRECIO=request.form['precioVenta']
+    p.EXISTENCIA=request.form['existencia']
+    p.COLOR=request.form['color']
+    p.MARCA=request.form['marca']
+    p.COSTOENVIO=request.form['costoEnvio']
+    estatus=request.values.get('estatus',False)
+    if estatus=="True":
+        p.ESTATUS=True
+    else:
+        p.ESTATUS=False
+    imagen=request.files['foto'].read()
+    if imagen:
+        p.foto=imagen
+    p.actualizar()
+    flash('Producto editado con éxito!!')
+    return render_template('Productos/editarProducto.html',producto=p)
 
 if __name__ == '__main__':
     db.init_app(app)
